aac5289723cb5e9fef3c856d3af95968e0d0bb4a
Nayan156 <nayandhawan.in@gmail.com>
2024-07-01 00:38:39 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const heading = React.createElement("h1", {id: "heading"}, "Hello World from React!");
+
+const parent = React.createElement("div",{id:"parent"},
+    React.createElement("div",{id: "child"},[
+        React.createElement("h1",{},"I'm a h1 tag"),
+        React.createElement("h2",{},"I'm a h2 tag")
+        ]
+    )
+);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render([heading, parent]);
