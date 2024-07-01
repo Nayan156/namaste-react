2678f2032247b652e5ba1fd318b8e1164c81722d
Nayan156 <nayandhawan.in@gmail.com>
2024-07-01 23:44:56 +0530

    Episode 3

--- App.js
+++ App.js
@@ -11,6 +11,22 @@ const parent = React.createElement("div",{id:"parent"},
     )
 ); // Creating React element using React
 
-const jsxHeading = <h1 id="heading" >Heading form React 🚀</h1>;  // Creating React element using JSX
+
+//This is React Element
+const jsxHeading = <h1 id="heading" >Namaste React form React Element 🚀</h1>;  // Creating React element using JSX 
+
+const HeadingComponent = () => <h1 id="heading"> Namaste React from React Function Component 🚀</h1>;
+const Title = () => (
+    <div className="container">
+        {/* Here is the prime example of Component Composition */}
+        <HeadingComponent /> 
+        <h1>Title after Component Composition</h1>
+        {/* JSX allows any Javascipt code inside it using "{}" thses parentheses, example below */}
+        {heading}
+    </div>
+)
+
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(jsxHeading);
+root.render(<Title />); // This is how a Component is Rendered
+// root.render(jsxHeading); //This is how a React Element is Rendered
